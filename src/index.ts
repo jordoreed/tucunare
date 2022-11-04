79997849a6722af406335dca9cae9debc75bf647
Jordan Reed <reedjordo@gmail.com>
2022-11-03 21:23:48 -0700

    stuff

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { vec4 } from './vec4';
 export { vec3 } from './vec3';
 export { vec2 } from './vec2';
 export * from './math';
-export { VertexShader, FragmentShader } from './shader';
+export type { VertexShader, FragmentShader } from './shader';
